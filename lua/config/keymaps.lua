75acb7ddc40ea20f274d4e8f0f130d1cf2ea8ceb
hugocharels <hugocharels@hotmail.com>
2023-11-28 14:55:29 +0000

    shortcut added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,36 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-----------------------------------------------------------
+-- Define keymaps of Neovim and installed plugins.
+-----------------------------------------------------------
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Change leader to a comma
+vim.g.mapleader = ","
+
+-----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
+
+-- Ctrl + S to save in all modes
+map("n", "<C-s>", ":w<CR>")
+map("i", "<C-s>", "<ESC>:w<CR>")
+
+-- Ctrl + Q to quit
+map("n", "<C-q>", ":q<CR>")
+map("i", "<C-q>", "<ESC>:q<CR>")
+
+-- Ctrl + Z to undo
+map("i", "<C-z>", ":undo<CR>")
+
+-- Ctrl + Shift + Z to redo
+map("i", "<C-S-z>", ":redo<CR>")
